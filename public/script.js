b05609be208afb2de0bfd5c2ed68a618efa6d2d4
Andrey Savchenko <contact@rarst.net>
2020-05-18 14:38:01 +0300

    Extracted search input to a component.

--- public/script.js
+++ public/script.js
@@ -109,55 +109,29 @@ Vue.component('App',
         search: '',
         types: [],
         results: [],
-        more: [],
-        lastScrollPosition: 0,
-        scrollingDown: false
+        more: []
       }
     },
     template: `
 <div>
-    <label class="sr-only" for="search">Search</label>
-        <div :class="headerClasses" class="flex items-center relative sticky top-0 pt-3 transition-opacity duration-300 rounded-full" id="header">
-            <input v-model="search" ref="searchInput" :disabled="loading" :placeholder="loading ? 'loading data…' : 'type a search query…'" class="border border-gray-600 placeholder-gray-600 w-full rounded-full py-2 px-6 outline-none focus:border-blue-300" type="text" id="search"/>
-            <button v-show="search" @click="reset(true)" class="absolute right-0 rounded-full text-gray-600 focus:outline-none hover:text-red-800 hover:bg-red-200 p-2 mr-1" title="Clear (Esc)">
-                <svg class="w-4 h-4" id="i-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3.5">
-                    <path d="M2 30 L30 2 M30 30 L2 2"/>
-                </svg>
-            </button>
-        </div>
-    
-        <div v-show="!results.length" class="flex flex-wrap justify-center mt-3 px-6 text-lg">
-            <type-link v-for="type in types" :type="type" :key="type"></type-link>
-        </div>
-    
-        <search-item :is="componentType(item.search_type)" v-for="item in results" :item="item" :key="item.key"></search-item>
+  <search-input v-model="search" ref="input" :loading="loading" @clear="reset(true)"></search-input>
+
+  <div v-show="!results.length" class="flex flex-wrap justify-center mt-3 px-6 text-lg">
+      <type-link v-for="type in types" :type="type" :key="type"></type-link>
+  </div>
+
+  <search-item :is="componentType(item.search_type)" v-for="item in results" :item="item" :key="item.key"></search-item>
 </div>`,
     created () {
       this.init()
 
-      window.addEventListener('popstate', () => this.navigate())
+      window.addEventListener('popstate', this.navigate)
       window.addEventListener('keyup', e => e.key === 'Escape' && this.reset(true))
-      window.addEventListener('scroll', () => {
-        this.scrollingDown = window.scrollY > this.lastScrollPosition
-        this.lastScrollPosition = window.scrollY
-
-        if (this.more.length &&
-          document.documentElement.scrollHeight - (window.scrollY + window.innerHeight) < 100) {
-          this.loadMore()
-        }
-      }, { passive: true })
+      window.addEventListener('scroll', this.handleScroll, { passive: true })
     },
     watch: {
       search: function (query) {
         this.runSearch(query)
-      }
-    },
-    computed: {
-      headerClasses () {
-        return {
-          'opacity-0': this.scrollingDown,
-          'shadow-md': this.lastScrollPosition > 0
-        }
       }
     },
     methods: {
@@ -170,6 +144,12 @@ Vue.component('App',
           this.types.sort()
           this.loading = false
         })
+      },
+      handleScroll () {
+        if (this.more.length &&
+          document.documentElement.scrollHeight - (window.scrollY + window.innerHeight) < 100) {
+          this.loadMore()
+        }
       },
       runSearch (query) {
         query = query.replace(/%20/g, ' ')
@@ -203,7 +183,7 @@ Vue.component('App',
         if (resetInput) {
           window.location.hash = ''
           this.search = ''
-          this.$refs.searchInput.focus()
+          this.$refs.input.focus()
         }
       },
       componentType (type) {
@@ -212,6 +192,50 @@ Vue.component('App',
     }
   }
 )
+
+Vue.component('search-input', {
+  created () {
+    window.addEventListener('scroll', this.handleScroll, { passive: true })
+  },
+  data () {
+    return {
+      lastScrollPosition: 0,
+      scrollingDown: false
+    }
+  },
+  props: ['value', 'loading'],
+  template: `
+  <div :class="headerClasses" class="flex items-center relative sticky top-0 pt-3 transition-opacity duration-300 rounded-full">
+    <label class="sr-only" for="search">Search</label>
+    <input :value="value" ref="input" :disabled="loading" :placeholder="placeholder" @input="$emit('input', $event.target.value)" class="border border-gray-600 placeholder-gray-600 w-full rounded-full py-2 px-6 outline-none focus:border-blue-300" type="text" id="search"/>
+    <button v-show="value" @click="$emit('clear')" class="absolute right-0 rounded-full text-gray-600 focus:outline-none hover:text-red-800 hover:bg-red-200 p-2 mr-1" title="Clear (Esc)">
+        <svg class="w-4 h-4" id="i-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3.5">
+            <path d="M2 30 L30 2 M30 30 L2 2"/>
+        </svg>
+    </button>
+  </div>`,
+  computed: {
+    headerClasses () {
+      return {
+        'opacity-0': this.scrollingDown,
+        'shadow-md': this.lastScrollPosition > 0
+      }
+    },
+    placeholder () {
+      return this.loading ? 'loading data…' : 'type a search query…'
+    }
+  },
+  methods: {
+    focus () {
+      this.$refs.input.focus()
+    },
+    handleScroll () {
+      this.scrollingDown = window.scrollY > this.lastScrollPosition
+      this.lastScrollPosition = window.scrollY
+    }
+  }
+})
+
 // endregion
 
 // region ITEMS
