ad4166c14b3a12b52d34bb1f349147c10893ece2
Andrey Savchenko <contact@rarst.net>
2022-06-14 22:13:50 +0300

    Added initial test

---
+++ codecept.conf.js
@@ -0,0 +1,18 @@
+exports.config = {
+  tests: './tests/*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost:3000/',
+      show: true,
+      browser: 'chromium',
+      waitForNavigation: "networkidle"
+    }
+  },
+  include: {
+
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'dnd'
+}
---
+++ tests/homepage_test.js
@@ -0,0 +1,33 @@
+Feature('homepage')
+
+Scenario('test homepage', ({ I }) => {
+
+  const content = {
+    abilities: 'Charisma',
+    classes: 'Barbarian',
+    conditions: 'Blinded',
+    'damage types': 'Acid',
+    equipment: 'Abacus',
+    features: 'Ability Score Improvement',
+    languages: 'Abyssal',
+    'magic items': 'Adamantine Armor',
+    'magic schools': 'Abjuration',
+    monsters: 'Aboleth',
+    proficiencies: 'All armor',
+    properties: 'Ammunition',
+    races: 'Dragonborn',
+    skills: 'Acrobatics',
+    spells: 'Acid Arrow',
+    subclasses: 'Berserker Barbarian',
+    subraces: 'High Elf',
+    traits: 'Artificer\'s Lore'
+  }
+
+  I.amOnPage('/')
+
+  for (let [link, header] of Object.entries(content)) {
+    I.pressKey('Escape')
+    I.click(link)
+    I.see(header)
+  }
+})
