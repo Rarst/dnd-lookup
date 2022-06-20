a68ded10e7a1218d66907030702954afaad89a0c
Andrey Savchenko <contact@rarst.net>
2022-06-20 18:41:07 +0300

    Adjusted custom color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js}"],
   theme: {
     extend: {
       colors: {
-        logo: "#ed1c24",
-        statblock: "#9c2b1b",
-        header: "#58180d",
         paper: "#f2ece4",
         "green-bg": "#e0e5c1",
-        deco: {
+        decoration: {
           100: "#e7d29b",
           200: "#c9ad6a",
           300: "#b89a67",
+        },
+        red: {
+          logo: "#ed1c24",
+          statblock: "#9c2b1b",
+          header: "#58180d",
         },
       },
     },
