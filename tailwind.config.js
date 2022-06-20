19d28c2cf1d9f2b6a4cd1bf9bf2522d1701a1309
Andrey Savchenko <contact@rarst.net>
2022-06-20 13:45:16 +0300

    Added DnD colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        logo: '#ed1c24',
+        statblock: '#9c2b1b',
+        header: '#58180d',
+        paper: '#f2ece4',
+        'green-bg': '#e0e5c1',
+        deco: {
+          100: '#e7d29b',
+          200: '#c9ad6a',
+          300: '#b89a67'
+        },
+      }
+    },
   },
   plugins: [],
 }
