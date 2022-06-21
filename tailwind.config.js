4c1fa99a0bb0d9be7a874a1485f420878060759c
Andrey Savchenko <contact@rarst.net>
2022-06-21 18:38:29 +0300

    Dropped custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,10 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js}"],
   theme: {
     extend: {
-      colors: {
-        paper: "#f2ece4",
-        "green-bg": "#e0e5c1",
-        decoration: {
-          100: "#e7d29b",
-          200: "#c9ad6a",
-          300: "#b89a67",
-        },
-        red: {
-          logo: "#ed1c24",
-          statblock: "#9c2b1b",
-          header: "#58180d",
-        },
       },
     },
   },
