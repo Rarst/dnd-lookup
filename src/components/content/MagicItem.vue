32a4730817c572c560bd2c7513f6cf19aedf8d35
Andrey Savchenko <contact@rarst.net>
2022-06-22 16:35:30 +0300

    Adjusted magic item type and rarity

--- src/components/content/MagicItem.vue
+++ src/components/content/MagicItem.vue
@@ -1,6 +1,11 @@
 <template>
-  <ItemHeader :item="item"></ItemHeader>
-  <p v-for="paragraph in item.desc" :key="paragraph">{{ paragraph }}</p>
+  <div>
+    <ItemHeader :item="item"></ItemHeader>
+    <p class="italic">{{ item.desc[0] }}</p>
+  </div>
+  <p v-for="paragraph in item.desc.slice(1)" :key="paragraph">
+    {{ paragraph }}
+  </p>
   <p>
     <strong class="font-semibold">Category:</strong>
     <ItemLink :linkTo="item.equipment_category.name"></ItemLink>
