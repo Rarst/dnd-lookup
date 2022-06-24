2c1cf684c75494651936d1f632224e0fb20e44c4
Andrey Savchenko <contact@rarst.net>
2022-06-24 20:54:01 +0300

    Restored class spellcasting for new data structure
    Fixes #3

--- src/components/content/Class.vue
+++ src/components/content/Class.vue
@@ -5,11 +5,14 @@
     <strong class="font-semibold">Saving Throws Proficiencies:</strong>
     {{ item.saving_throws.map((st) => st.name).join(", ") }}
   </p>
-  <!--    <p v-if="item.spellcasting.class" class="-ml-1"><item-link :linkTo="'Spellcasting ('+item.spellcasting.class+')'"></item-link></p>-->
-  <LinksSection :item="item" section="proficiencies"></LinksSection>
-  <div v-if="item.proficiency_choices.length">
-    <h3 class="text-xl font-bold">Proficiency options</h3>
-    <div v-for="option in item.proficiency_choices">
+
+  <div>
+    <h3 class="text-lg font-bold">Proficiencies</h3>
+    <LinksSection :item="item" header="" section="proficiencies"></LinksSection>
+    <div
+      v-if="item.proficiency_choices.length"
+      v-for="option in item.proficiency_choices"
+    >
       <h4 class="font-bold">Choose {{ option.choose }}</h4>
       <p class="-ml-1 flex flex-wrap">
         <ItemLink
@@ -21,7 +24,19 @@
     </div>
   </div>
 
-  <links-section :item="item" section="subclasses"></links-section>
+  <template v-if="item.spellcasting">
+    <h3 class="text-lg font-bold">Spellcasting</h3>
+    <p>
+      <strong class="font-semibold">Level</strong>
+      {{ item.spellcasting.level }}
+    </p>
+    <template v-for="info in item.spellcasting.info">
+      <h4 class="font-bold">{{ info.name }}</h4>
+      <p v-for="paragraph in info.desc">{{ paragraph }}</p>
+    </template>
+  </template>
+
+  <LinksSection :item="item" section="subclasses"></LinksSection>
 </template>
 
 <script>
@@ -34,18 +49,4 @@ export default {
 
   props: ["item"],
 };
-
-// TODO spellcasting
-// Vue.component('spellcasting', {
-//   props: ['item'],
-//   template: `
-// <div class="mt-3 border rounded-lg py-3 px-6 space-y-3">
-//     <item-header :item="item"></item-header>
-//     <p><strong class="font-semibold">Level</strong> {{item.level}}</p>
-//     <template v-for="info in item.info">
-//         <h3 class="font-bold">{{info.name}}</h3>
-//         <p v-for="paragraph in info.desc">{{paragraph}}</p>
-//     </template>
-// </div>`
-// })
 </script>
