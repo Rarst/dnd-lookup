bb77e30d3a2eae3c41de473cc84a7d06f9832acc
Andrey Savchenko <contact@rarst.net>
2022-06-29 20:38:09 +0300

    Reverted converting to non-breaking space in links
    Unnecessary with inline block.

--- src/components/ItemLink.vue
+++ src/components/ItemLink.vue
@@ -2,7 +2,7 @@
   <a
     class="m-1 inline-block rounded-lg bg-stone-200 py-1 px-2 font-serif font-semibold transition-colors duration-100 hover:bg-red-100 hover:text-red-700"
     :href="'#' + link"
-    >{{ link.replace(" ", "&nbsp;") }}</a
+    >{{ link }}</a
   >
 </template>
 
