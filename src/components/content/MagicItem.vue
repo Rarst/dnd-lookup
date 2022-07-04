b04c16fef7fc8f9d3ad815c06e08acaf5931f057
Andrey Savchenko <contact@rarst.net>
2022-07-04 15:41:57 +0300

    Parsed markdown in magic items

--- src/components/content/MagicItem.vue
+++ src/components/content/MagicItem.vue
@@ -3,9 +3,10 @@
     <ItemHeader :item="item"></ItemHeader>
     <p class="italic">{{ item.desc[0] }}</p>
   </div>
-  <p v-for="paragraph in item.desc.slice(1)" :key="paragraph">
-    {{ paragraph }}
-  </p>
+  <div
+    v-html="content"
+    class="prose prose-stone max-w-none prose-p:leading-normal prose-li:leading-normal prose-ol:sm:-ml-8 prose-ul:sm:-ml-8"
+  ></div>
   <p>
     <strong class="font-semibold">Category:</strong>
     <ItemLink :linkTo="item.equipment_category.name"></ItemLink>
@@ -15,9 +16,27 @@
 <script>
 import ItemHeader from "../ItemHeader.vue";
 import ItemLink from "../ItemLink.vue";
+import { marked } from "marked";
 
 export default {
   components: { ItemLink, ItemHeader },
   props: ["item"],
+  computed: {
+    content() {
+      let content = [];
+
+      for (let sentence of this.item.desc.slice(1)) {
+        if (sentence[0] === "-" || sentence[0] === "|") {
+          // unordered list item or table row
+          content.push("\n" + sentence);
+        } else {
+          // paragraph or another standalone element
+          content.push("\n\n" + sentence);
+        }
+      }
+
+      return marked.parse(content.join(""));
+    },
+  },
 };
 </script>
