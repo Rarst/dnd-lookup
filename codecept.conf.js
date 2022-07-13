412b37b716944c9690a1a8fb210bf5666c112e26
Andrey Savchenko <contact@rarst.net>
2022-07-13 17:15:09 +0300

    Updated to Vite v3

--- codecept.conf.js
+++ codecept.conf.js
@@ -7,7 +7,7 @@ exports.config = {
   output: "./output",
   helpers: {
     Playwright: {
-      url: "http://localhost:3000/",
+      url: "http://localhost:5173",
       show: true,
       browser: "chromium",
       waitForNavigation: "networkidle",
