c2d970232d49f525eb16840dde24040c2127baca
Andrey Savchenko <contact@rarst.net>
2022-07-25 15:32:39 +0300

    Initial vite-ssg attempt

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import { createApp } from "vue";
 import App from "./App.vue";
+import { ViteSSG } from "vite-ssg/single-page";
 import ItemHeader from "./components/ItemHeader.vue";
 import ItemLink from "./components/ItemLink.vue";
 import LinksSection from "./components/LinksSection.vue";
 import Markdown from "./components/Markdown.vue";
 import "./index.css";
 
-if ("serviceWorker" in navigator) {
+if (typeof navigator !== "undefined" && "serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/service-worker.js");
   });
 }
 
-const Vue = createApp(App);
+const Vue = ViteSSG(App);
 
 Vue.component("ItemHeader", ItemHeader);
 Vue.component("ItemLink", ItemLink);
